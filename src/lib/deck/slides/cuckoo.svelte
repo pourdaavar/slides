64b030e0d5caf113e2f4d0db48d1911088f4a207
Hossein Pourdavar <h.pourdavar@gmail.com>
2023-12-22 21:14:01 +0330

    3

--- src/lib/deck/slides/cuckoo.svelte
+++ src/lib/deck/slides/cuckoo.svelte
@@ -17,7 +17,9 @@
 		<p data-id="x" class="text-2xl">
 			CS is one of the latest nature-inspired metaheuristic algorithms,
 
-			<span>developed in 2009 <span class="text-pink-600">by Xin-She Yang and Suash Deb</span></span
+			<span
+				>developed in 2009 <span class="text-pink-600"><br />by Xin-She Yang and Suash Deb</span
+				></span
 			>
 		</p>
 	</section>
@@ -28,7 +30,9 @@
 		<p data-id="x" class="text-2xl">
 			CS is one of the latest nature-inspired metaheuristic algorithms,
 
-			<span>developed in 2009 <span class="text-pink-600">by Xin-She Yang and Suash Deb</span></span
+			<span
+				>developed in 2009 <span class="text-pink-600"><br />by Xin-She Yang and Suash Deb</span
+				></span
 			>
 		</p>
 
@@ -36,5 +40,28 @@
 			<img src="./author.png" class="size-40" alt="Xin-She Yang" />
 			<span class="text-lg">Xin-She Yang</span>
 		</div>
+	</section>
+
+	<section data-auto-animate>
+		<p>Cuckoo Search</p>
+
+		<p data-id="x" class="text-2xl">
+			CS is one of the latest nature-inspired metaheuristic algorithms,
+
+			<span
+				>developed in 2009 <span class="text-pink-600"><br />by Xin-She Yang and Suash Deb</span
+				></span
+			>
+		</p>
+
+		<div class="flex flex-col items-center justify-center">
+			<img src="./author.png" class="size-40" alt="Xin-She Yang" />
+			<span class="text-lg">Xin-She Yang</span>
+		</div>
+
+		<p class="text-2xl">
+			This algorithm is enhanced by the so-called Lévy flights rather than by simple isotropic
+			random walks.
+		</p>
 	</section>
 </section>
