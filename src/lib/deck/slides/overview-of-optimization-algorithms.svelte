1907bcf53205a02564d7eb43b116156d7fe6948a
Hossein Pourdavar <h.pourdavar@gmail.com>
2023-12-22 18:53:53 +0330

    overview done

--- src/lib/deck/slides/overview-of-optimization-algorithms.svelte
+++ src/lib/deck/slides/overview-of-optimization-algorithms.svelte
@@ -1,5 +1,75 @@
-<section>
+<section class="text-left">
 	<section data-auto-animate>
-		<div>2. Overview of Optimization Algorithms</div>
+		<div data-id="title-2">2. Overview of Optimization Algorithms</div>
+	</section>
+
+	<section data-auto-animate>
+		<div data-id="title-2">2. Overview of Optimization Algorithms</div>
+
+		<div data-id="fuck">
+			<p class="text-3xl font-medium text-pink-500">What are Optimization Algorithms?</p>
+
+			<p class="text-xl font-light">
+				Optimization algorithms are <span class="fuck-p">computational methods</span>
+				designed to find the <span class="fuck-p">best solution</span> to a problem from
+				<span class="fuck-p">a set of possible solutions</span>. <br />The goal is to maximize or
+				minimize a specific objective function while satisfying given constraints.
+			</p>
+		</div>
+	</section>
+
+	<section data-auto-animate>
+		<div data-id="fuck">
+			<p class="text-5xl font-medium text-pink-500">Key Components of Optimization Algorithms:</p>
+
+			<div class="text-2xl">
+				<p>1- Objective Function:</p>
+				<ul class="text-lg">
+					<li>Defines the goal to be optimized</li>
+				</ul>
+
+				<p>2- Decision Variables:</p>
+				<ul class="text-lg">
+					<li>Parameters or variables that affect the outcome of the objective function.</li>
+					<li>Values of these variables are adjusted to optimize the objective.</li>
+				</ul>
+
+				<p>3- Constraints:</p>
+				<ul class="text-lg">
+					<li>Conditions or limitations that must be satisfied</li>
+					<li>Ensures that solutions adhere to specific requirements.</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<section data-auto-animate>
+		<div data-id="fuck">
+			<p class="text-5xl font-medium text-pink-500">Types of Optimization Algorithms:</p>
+
+			<div class="text-2xl">
+				<p>1- Evolutionary Algorithms:</p>
+				<ul class="text-lg">
+					<li>Genetic algorithms</li>
+				</ul>
+
+				<p>2- Metaheuristic Algorithms</p>
+				<ul class="text-lg">
+					<li>PSO</li>
+					<li>AOC</li>
+					<li>Cuckoo Search</li>
+				</ul>
+			</div>
+		</div>
 	</section>
 </section>
+
+<style>
+	.fuck-p {
+		@apply font-semibold text-pink-600;
+	}
+
+	p + ul {
+		@apply ml-16;
+	}
+</style>
