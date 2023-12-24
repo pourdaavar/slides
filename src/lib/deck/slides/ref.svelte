a36a69e1926837970692313b52fb6b163b416218
Hossein Pourdavar <h.pourdavar@gmail.com>
2023-12-25 00:30:35 +0330

    f

--- src/lib/deck/slides/ref.svelte
+++ src/lib/deck/slides/ref.svelte
@@ -2,7 +2,8 @@
 	<p>References</p>
 
 	<div class="text-2xl">
+        <div>All content has been taken from Nature-inspired Optimization Algorithms written by Xin-She Yang</div>
 		<div>Bird image: pixabay.com</div>
-		<div>Video of cukcoo: BBC Earth</div>
+		<div>Video of cuckoo: BBC Earth</div>
 	</div>
 </section>
